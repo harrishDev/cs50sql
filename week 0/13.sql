3d301ed3af7234bf97de27219acea1f89355378f
Harrish <harrishoffical@gmail.com>
2024-01-06 21:40:57 +0530

    Problem Set 0

---
+++ week 0/13.sql
@@ -0,0 +1,2 @@
+SELECT "id" FROM "episodes"
+WHERE ("id" = '1' OR "id" = '2') AND "id" = '140';
---
+++ week 0/9.sql
@@ -0,0 +1,2 @@
+SELECT COUNT("episodes") FROM "episodes"
+WHERE ("air_date" >= '2002-01-01' AND "air_date" <= '2007-12-31');
