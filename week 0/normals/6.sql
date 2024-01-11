97f2cd58f83e7bda5fc3de0eea726a98b7cfd53e
Harrish <harrishoffical@gmail.com>
2024-01-11 21:55:42 +0530

    normals

---
+++ week 0/normals/6.sql
@@ -0,0 +1,2 @@
+SELECT "50m", "latitude", "longitude" FROM "normals"
+WHERE ("latitude" > 0 AND "latitude" < 20) AND ("longitude" > 55 AND "longitude" < 75);
---
+++ week 0/normals/9.sql
@@ -0,0 +1,4 @@
+SELECT "0m", "longitude","latitude"
+FROM "normals"
+ORDER BY "0m" DESC, "latitude"
+LIMIT 10;
